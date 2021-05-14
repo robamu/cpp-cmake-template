6556f2cd9497273b3a509f1241a26fb2ac4b7bcb
Robin Mueller <robin.mueller.m@gmail.com>
2021-05-14 23:49:35 +0200

    small tweak

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <boost/lambda/lambda.hpp>
+#include <iterator>
+#include <algorithm>
 
 int main() {
 	std::cout << "Hello world!" << std::endl;
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::for_each(in(std::cin), in(), std::cout << (_1 * 3) << " ");
+	std::endl;
 	return 0;
 }
